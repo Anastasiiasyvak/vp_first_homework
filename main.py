d43151271c1651cffa8613d10e6a19e2349170d4
Anastasiiasyvak <asyvak@kse.org.ua>
2023-11-22 22:13:58 +0200

    data cleaning

--- main.py
+++ main.py
@@ -11,3 +11,30 @@ print(wine_data.info())  # my general info about dataset(number of rows and colu
 
 print(wine_data.isnull().sum()) # This command displays the number of missing values in each column
 # the isnull() true -> for NaN false->for not NaN, sum() converts false to zero so in my case I don't have missing values
+
+#===================================================
+
+# Data Cleaning
+
+# so here we replace all NaN values in the ABV % column with the average value of this column
+wine_data['ABV %'].fillna(wine_data['ABV %'].mean(), inplace=True)
+
+
+#====================================================
+
+# data vizualization
+
+
+#name - name of the wine
+#color_wine - color of the wine
+# Prices - price of the wine
+# ML - Wine volume in millilitres
+# Ratings - rating of wine
+# Ratingsnum - Number of ratings determining the ratings
+# Countrys - country of wine
+# ABV% - The alcohol content of wine is shown as a percentage
+# rates - addition ratings or information
+
+
+
+
