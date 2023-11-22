02f55ffd21abe8d69f26f271038c9bbedf60a0bd
Anastasiiasyvak <asyvak@kse.org.ua>
2023-11-22 22:58:58 +0200

    bar chart

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+plt.switch_backend('TkAgg')  # for creating a window of our graphics
 
 # my file has coding UTF-16, so I point it like this
 wine_data = pd.read_csv('vivno_dataset.csv', encoding='UTF-16')
 
-print(wine_data.head()) # we use this to first of all understand whether
-# we have access to our data and also to see if the date is displayed correctly
-# (namely the first 5 lines)
-
-print(wine_data.info())  # my general info about dataset(number of rows and columns, data types..)
-
-print(wine_data.isnull().sum()) # This command displays the number of missing values in each column
-# the isnull() true -> for NaN false->for not NaN, sum() converts false to zero so in my case I don't have missing values
+# print(wine_data.head()) # we use this to first of all understand whether
+# # we have access to our data and also to see if the date is displayed correctly
+# # (namely the first 5 lines)
+#
+# print(wine_data.info())  # my general info about dataset(number of rows and columns, data types..)
+#
+# print(wine_data.isnull().sum()) # This command displays the number of missing values in each column
+# # the isnull() true -> for NaN false->for not NaN, sum() converts false to zero so in my case I don't have missing values
 
 #===================================================
 
@@ -36,5 +38,19 @@ wine_data['ABV %'].fillna(wine_data['ABV %'].mean(), inplace=True)
 # rates - addition ratings or information
 
 
+# bar chart
+wine_data['color_wine'].value_counts().plot(kind='bar', color=['skyblue', 'lightcoral']) # count our color_wine and realization of it
+plt.title('Distribution of Wine Colors') # the name
+plt.xlabel('Wine Color') # for x
+plt.ylabel('Count') # for y
+
+
+# adding annotations (text labels) to each column
+for i, count in enumerate(wine_data['color_wine'].value_counts()):
+    plt.text(i, count + 0.1, str(count), ha='center', va='bottom')
+
+plt.show() # creating and show this bar chart
+
+# line chart
 
 
